dcc9b560c99cf0b1a48bd44d6cc15f84dfd52b99
Emil Sommerfeldt <emil@jungfeldt.de>
2019-02-06 18:21:10 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -26,109 +26,7 @@
                     </button>
                 </div>
                 <div v-if="error" class="results">
-                    <app-panel>
-                        <app-table>
-                            <app-table-body>
-                                <app-table-row extend v-if="results.tx">
-                                    <app-table-row-cell extend>
-                                        <app-definition
-                                                type="list"
-                                                title="tx"
-                                        >
-                                            {{ results.tx }}
-                                        </app-definition>
-                                    </app-table-row-cell>
-                                </app-table-row>
-                                <app-table-row extend v-if="results.signature">
-                                    <app-table-row-cell extend>
-                                        <app-definition
-                                                type="list"
-                                                title="Signature"
-                                        >
-                                            {{ results.signature }}
-                                            <p v-if="error.InvalidSignature" class="error">{{ error.InvalidSignature }}</p>
-                                        </app-definition>
-                                    </app-table-row-cell>
-                                </app-table-row>
-                                <app-table-row extend>
-                                    <app-table-row-cell extend>
-                                        <app-definition
-                                                type="list"
-                                                title="Sender account"
-                                        >
-                                            {{ results.txObject.senderId }}
-                                        </app-definition>
-                                    </app-table-row-cell>
-                                </app-table-row>
-                                <app-table-row extend>
-                                    <app-table-row-cell extend>
-                                        <app-definition
-                                                type="list"
-                                                title="Recipient account"
-                                        >
-                                            {{ results.txObject.recipientId }}
-                                        </app-definition>
-                                    </app-table-row-cell>
-                                </app-table-row>
-                                <app-table-row>
-                                    <app-table-row-cell extend>
-                                        <app-definition
-                                                type="list"
-                                                title="Amount"
-                                        >
-                                            {{ results.txObject.amount }}
-                                            <p v-if="warning.InsufficientBalanceForAmount" class="warning">{{ warning.InsufficientBalanceForAmount }}</p>
-                                        </app-definition>
-                                    </app-table-row-cell>
-                                </app-table-row>
-                                <app-table-row>
-                                    <app-table-row-cell extend>
-                                        <app-definition
-                                                type="list"
-                                                title="Ttl"
-                                        >
-                                            {{ results.txObject.ttl }}
-                                            <p v-if="error.ExpiredTTL" class="error">{{ error.ExpiredTTL }}</p>
-                                        </app-definition>
-                                    </app-table-row-cell>
-                                </app-table-row>
-                                <app-table-row>
-                                    <app-table-row-cell extend>
-                                        <app-definition
-                                                type="list"
-                                                title="Fee"
-                                        >
-                                            {{ results.txObject.fee }}
-                                            <p v-if="warning.InsufficientBalanceForAmountFee" class="warning">{{ warning.InsufficientBalanceForAmountFee }}</p>
-                                            <p v-if="error.InsufficientFee" class="error">{{ error.InsufficientFee }}</p>
-                                        </app-definition>
-                                    </app-table-row-cell>
-                                </app-table-row>
-                                <app-table-row>
-                                    <app-table-row-cell extend>
-                                        <app-definition
-                                                type="list"
-                                                title="Nonce"
-                                        >
-                                            {{ results.txObject.nonce }}
-                                            <p v-if="error.NonceUsed" class="error">{{ error.NonceUsed }}</p>
-                                            <p v-if="warning.NonceHigh" class="warning">{{ warning.NonceHigh }}</p>
-                                        </app-definition>
-                                    </app-table-row-cell>
-                                </app-table-row>
-                                <app-table-row extend>
-                                    <app-table-row-cell extend>
-                                        <app-definition
-                                                type="list"
-                                                title="Payload"
-                                        >
-                                            {{ results.txObject.payload }}
-                                        </app-definition>
-                                    </app-table-row-cell>
-                                </app-table-row>
-                            </app-table-body>
-                        </app-table>
-                    </app-panel>
+                    <gogglesResults :error="error" :results="results"/>
                 </div>
             </div>
 
@@ -151,12 +49,6 @@
 </template>
 
 <script>
-    import AppTableRow from './components/appTableRow'
-    import AppTableRowCell from './components/appTableRowCell'
-    import AppDefinition from './components/appDefinition'
-    import AppTable from './components/appTable'
-    import AppTableBody from './components/appTableBody'
-    import AppPanel from './components/appPanel'
     import AeLogo from './components/aeLogo'
     import GogglesLogo from "./components/gogglesLogo";
     import {AeLoader} from '@aeternity/aepp-components'
@@ -164,6 +56,7 @@
     import TxValidator from '@aeternity/aepp-sdk/es/tx/validator'
     import * as TxBuilder from '@aeternity/aepp-sdk/es/tx/builder'
     import * as TxHelper from '@aeternity/aepp-sdk/es/tx/builder/helpers'
+    import GogglesResults from "./GogglesResults";
 
 
     const NODE_URL = 'https://sdk-mainnet.aepps.com';
@@ -173,13 +66,8 @@
     export default {
         name: 'app',
         components: {
+            GogglesResults,
             GogglesLogo,
-            AppTableRow,
-            AppTableRowCell,
-            AppDefinition,
-            AppTable,
-            AppTableBody,
-            AppPanel,
             AeLoader,
             AeLogo
         },
@@ -413,20 +301,6 @@
             margin-top: .5rem;
             margin-bottom: -.5rem;
             text-align: center;
-        }
-    }
-
-    .warning {
-        color: #5F4191;
-        font-size: .7rem;
-        font-family: "Inter UI", sans-serif;
-        background-color: #F7FAFC;
-        font-weight: bold;
-        width: 100%;
-        margin-top: 0;
-        opacity: .8;
-        @media (min-width: 450px) {
-            background-color: #fff;
         }
     }
 
